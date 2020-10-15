a76b41ceafe1dd990fb8d6c1babcf8aa2ffeca96
daichi08 <b1504000@planet.kanazawa-it.ac.jp>
2020-10-15 18:25:06 +0900

    foregroundに一本化

--- MyApplication3/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplication3/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -24,16 +24,14 @@ import java.util.*
 import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
+    val REQUEST_ENABLE_BT = 160
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val bluetoothManager: BluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
-        val bluetoothAdapter: BluetoothAdapter? = bluetoothManager.adapter
-        val REQUEST_ENABLE_BT: Int = 1
+        /** パーミッションの確認 **/
         val REQUEST_CODE = 150
-        val SERVICE_UUID = ParcelUuid(UUID.fromString("895D1816-CC8A-40E3-B5FC-42D8ED441E50"))
-        val DATA_UUID = ParcelUuid(UUID.fromString("00004376-0000-1000-8000-00805F9B34FB"))
         val permissions = arrayOf(
             Manifest.permission.BLUETOOTH,
             Manifest.permission.BLUETOOTH_ADMIN,
@@ -47,64 +45,113 @@ class MainActivity : AppCompatActivity() {
             return
         }
 
-        when{
-            (bluetoothAdapter == null) -> {
+        val bluetooth = BluetoothClass(this)
+        val randomHex = (0..0xFFFFFFF).random().toString()
+        val advertiseIntent = Intent(this, BleScanService::class.java)
+
+        when(bluetooth.request()){
+            bluetooth.BT_NOT_SUPPORTED -> {
+                Toast.makeText(this, "BT not Supported", Toast.LENGTH_SHORT).show()
                 finish()
             }
-            (packageManager.hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) -> {
-                val randomHex = (0..0xFFFFFFF).random().toString()
-                /**
-                 * バックグラウンド処理用変数
-                 */
-                val requestCode = 240
-                val intent = Intent(this, BleScanReceiver::class.java)
-                val intent2 = Intent(this, BleScanService::class.java)
-                val pendingIntent = PendingIntent.getBroadcast(this, requestCode, intent, PendingIntent.FLAG_UPDATE_CURRENT)
-                startForegroundService(intent2)
-                /**
-                 * BLE受信用変数
-                 */
-                val scanner= bluetoothAdapter.bluetoothLeScanner
-                val scanFilter: ScanFilter = ScanFilter.Builder().setServiceUuid(SERVICE_UUID).build()
-                val scanFilterList: ArrayList<ScanFilter> = arrayListOf(scanFilter)
-                val scanSettings: ScanSettings = ScanSettings.Builder().setScanMode(
-                    ScanSettings.SCAN_MODE_BALANCED
-                ).build()
-
-                /**
-                 * BLE送信用変数
-                 */
-                val advertiser: BluetoothLeAdvertiser = bluetoothAdapter.bluetoothLeAdvertiser
-                val advertiseSettings = AdvertiseSettings.Builder()
-                    .setAdvertiseMode(AdvertiseSettings.ADVERTISE_MODE_LOW_POWER)
-                    .setConnectable(false)
-                    .setTimeout(0)
-                    .setTxPowerLevel(AdvertiseSettings.ADVERTISE_TX_POWER_MEDIUM).build()
-                val advertiseData: AdvertiseData = AdvertiseData.Builder()
-                    .addServiceUuid(SERVICE_UUID)
-                    .addServiceData(DATA_UUID, randomHex.toString().toByteArray(Charsets.UTF_8))
-                    .build()
-                val advertiseCallback: AdvertiseCallback =  object : AdvertiseCallback() {
-                    override fun onStartSuccess(settingsInEffect: AdvertiseSettings) {
-                        Log.d("adv", "success")
-                        super.onStartSuccess(settingsInEffect)
-                    }
-
-                    override fun onStartFailure(errorCode: Int) {
-                        Log.d("adv", errorCode.toString())
-                        super.onStartFailure(errorCode)
-                    }
-                }
-                /**
-                 * BLE送受信の開始
-                 */
-                scanner.startScan(scanFilterList, scanSettings, pendingIntent)
-                advertiser.startAdvertising(advertiseSettings, advertiseData, advertiseCallback)
-            }
-            else -> {
+            bluetooth.BLE_NOT_SUPPORTED -> {
+                Toast.makeText(this, "BLE not Supported", Toast.LENGTH_SHORT).show()
                 finish()
+            }
+            bluetooth.BLE_NOT_RUNNNING -> {
+                val enableBtIntent: Intent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
+                startForegroundService(advertiseIntent)
+            }
+            else ->{
+                startForegroundService(advertiseIntent)
             }
         }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        when(requestCode){
+            REQUEST_ENABLE_BT -> {
+                when(resultCode){
+                    Activity.RESULT_CANCELED -> {
+                        Toast.makeText(this,"BLE not running",Toast.LENGTH_LONG).show()
+                        finish()
+                    }
+                }
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+}
+
+class BluetoothClass(val context: Context){
+    private val SERVICE_UUID = ParcelUuid(UUID.fromString("895D1816-CC8A-40E3-B5FC-42D8ED441E50"))
+    private val DATA_UUID    = ParcelUuid(UUID.fromString("00004376-0000-1000-8000-00805F9B34FB"))
+    val bluetoothManager: BluetoothManager = context.getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+    val bluetoothAdapter: BluetoothAdapter? = bluetoothManager.adapter
+    val BT_NOT_SUPPORTED  = -3
+    val BLE_NOT_SUPPORTED = -2
+    val BLE_NOT_RUNNNING  = -1
+    val BLE_RUNNNING      = 0
+    val REQUEST_SCAN_BLE  = 170
+
+    fun request(): Int{
+        return when{
+            (bluetoothAdapter == null) -> {
+                BT_NOT_SUPPORTED
+            }
+            (!context.packageManager.hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) -> {
+                BLE_NOT_SUPPORTED
+            }
+            (bluetoothAdapter.isEnabled) -> {
+                BLE_RUNNNING
+            }
+            else -> {
+                BLE_NOT_RUNNNING
+            }
+        }
+    }
+
+    private fun startBleScan(){
+        val scanner= bluetoothAdapter!!.bluetoothLeScanner
+        val scanFilter: ScanFilter = ScanFilter.Builder().setServiceUuid(SERVICE_UUID).build()
+        val scanFilterList: ArrayList<ScanFilter> = arrayListOf(scanFilter)
+        val scanSettings: ScanSettings = ScanSettings.Builder().setScanMode(
+            ScanSettings.SCAN_MODE_LOW_POWER
+        ).build()
+        val scanIntent = Intent(context, BleScanReceiver::class.java)
+        val pendingIntent = PendingIntent.getBroadcast(context, REQUEST_SCAN_BLE, scanIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+        scanner.startScan(scanFilterList, scanSettings, pendingIntent)
+    }
+
+    private fun startBleAdvertise(){
+        val advertiser: BluetoothLeAdvertiser = bluetoothAdapter!!.bluetoothLeAdvertiser
+        val advertiseSettings = AdvertiseSettings.Builder()
+            .setAdvertiseMode(AdvertiseSettings.ADVERTISE_MODE_LOW_POWER)
+            .setConnectable(false)
+            .setTimeout(0)
+            .setTxPowerLevel(AdvertiseSettings.ADVERTISE_TX_POWER_MEDIUM).build()
+        val advertiseData: AdvertiseData = AdvertiseData.Builder()
+            .addServiceUuid(SERVICE_UUID)
+            .addServiceData(DATA_UUID, "aaaa".toByteArray(Charsets.UTF_8))
+            .build()
+        val advertiseCallback: AdvertiseCallback =  object : AdvertiseCallback() {
+            override fun onStartSuccess(settingsInEffect: AdvertiseSettings) {
+                Log.d("adv", "success")
+                super.onStartSuccess(settingsInEffect)
+            }
+
+            override fun onStartFailure(errorCode: Int) {
+                Log.d("adv", errorCode.toString())
+                super.onStartFailure(errorCode)
+            }
+        }
+        advertiser.startAdvertising(advertiseSettings, advertiseData, advertiseCallback)
+    }
+
+    fun startScanAndAdvertise(){
+        startBleScan()
+        startBleAdvertise()
     }
 }
 
@@ -130,6 +177,7 @@ class BleScanService : Service(){
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        super.onStartCommand(intent, flags, startId)
         val channelId = "my_service"
         val channelName = "My Background Service"
         val channel = NotificationChannel(
@@ -138,6 +186,8 @@ class BleScanService : Service(){
             NotificationManager.IMPORTANCE_NONE
         )
         val service = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val context = applicationContext
+        val bluetooth = BluetoothClass(context)
 
         channel.description = "Silent Notification"
         channel.setSound(null,null)
@@ -153,6 +203,8 @@ class BleScanService : Service(){
             .setPriority(PRIORITY_MIN)
             .setCategory(Notification.CATEGORY_SERVICE)
             .build()
+        bluetooth.startScanAndAdvertise()
+
         startForeground(101, notification)
 
         return START_NOT_STICKY
