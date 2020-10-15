e92ad202809163e9b5d47498c66b8b217b4459dd
daichi08 <b1504000@planet.kanazawa-it.ac.jp>
2020-10-15 20:21:14 +0900

    ログイン実装

--- MyApplication3/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplication3/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,26 +5,38 @@ import android.app.*
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothManager
 import android.bluetooth.le.*
-import android.companion.BluetoothLeDeviceFilter
 import android.content.BroadcastReceiver
+import android.content.ContentValues
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.IBinder
 import android.os.ParcelUuid
 import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationCompat.PRIORITY_MIN
+import com.github.kittinunf.fuel.core.Response
+import com.github.kittinunf.fuel.core.ResponseResultOf
+import com.github.kittinunf.fuel.httpPost
+import com.github.kittinunf.result.Result
+import com.google.gson.Gson
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
 import pub.devrel.easypermissions.EasyPermissions
 import java.util.*
 import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
-    val REQUEST_ENABLE_BT = 160
+    private val REQUEST_ENABLE_BT = 160
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -46,8 +58,10 @@ class MainActivity : AppCompatActivity() {
         }
 
         val bluetooth = BluetoothClass(this)
-        val randomHex = (0..0xFFFFFFF).random().toString()
-        val advertiseIntent = Intent(this, BleScanService::class.java)
+        val DB = DatabaseHelper(this)
+        val server = RailsServer()
+
+        DB.existDB()
 
         when(bluetooth.request()){
             bluetooth.BT_NOT_SUPPORTED -> {
@@ -61,10 +75,42 @@ class MainActivity : AppCompatActivity() {
             bluetooth.BLE_NOT_RUNNNING -> {
                 val enableBtIntent: Intent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
                 startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
-                startForegroundService(advertiseIntent)
             }
             else ->{
-                startForegroundService(advertiseIntent)
+                val advertiseIntent = Intent(this, BleScanService::class.java)
+                when{
+                    DB.existUser() -> {
+                        val user = DB.getUser()
+                        var (result, railsUser, errorMsg) = server.signIn(user)
+                        when{
+                            result -> {
+                                startForegroundService(advertiseIntent)
+                            }
+                            else -> { }
+                        }
+                    }
+                    else -> {
+                        val emailForm = findViewById<EditText>(R.id.emailForm)
+                        val passwordForm = findViewById<EditText>(R.id.passwordForm)
+                        val nameForm = findViewById<EditText>(R.id.nameForm)
+                        val btn = findViewById<Button>(R.id.btn)
+
+                        btn.setOnClickListener {
+                            val user: UserClass = UserClass(
+                                emailForm.text.toString(),
+                                passwordForm.text.toString(),
+                                nameForm.text.toString()
+                            )
+                            var (result, railsUser, errorMsg) = server.signUp(user, DB)
+                            when{
+                                result -> {
+                                    startForegroundService(advertiseIntent)
+                                }
+                                else -> { }
+                            }
+                        }
+                    }
+                }
             }
         }
     }
@@ -81,6 +127,107 @@ class MainActivity : AppCompatActivity() {
             }
         }
         super.onActivityResult(requestCode, resultCode, data)
+    }
+}
+
+class RailsServer(){
+    private val ROUTE_URL = "http://202.13.162.197:3000/"
+    // private val ROUTE_URL = "https://klab-api-server.herokuapp.com/"
+    private val SIGN_IN_URL: String = "auth/sign_in"
+    private val SIGN_UP_URL: String = "auth"
+    private val LOG_CREATE_URL: String = "log"
+    private val HTTP_CONFRICT = 409
+    lateinit var header: HeaderClass
+
+    private fun postJson(URL: String, body: String, head: HashMap<String, String> = hashMapOf("Content-Type" to "application/json")): ResponseResultOf<ByteArray> {
+        return runBlocking(Dispatchers.Default) {
+            (ROUTE_URL+URL).httpPost()
+                .header(head)
+                .body(body)
+                .response()
+        }
+    }
+
+    fun signIn(user: UserClass): Triple<Boolean, UserClass, HashMap<String?, String?>>{
+        val body = Gson().toJson(
+            hashMapOf(
+                "session" to
+                        hashMapOf(
+                            "email" to user.email,
+                            "password" to user.password
+                        )
+            )
+        )
+        val (_, response, result) = this.postJson(
+            SIGN_IN_URL,
+            body
+        )
+        return when(result){
+            is Result.Success -> {
+                val headerClass = HeaderClass(response)
+                val hashData = Gson().fromJson(
+                    response.data.toString(Charsets.UTF_8),
+                    HashMap::class.java
+                )
+                val serverData = Gson().fromJson(
+                    Gson().toJson(hashData["data"]),
+                    HashMap::class.java
+                )
+
+                val userClass = UserClass(
+                    user.email!!,
+                    user.password!!,
+                    serverData["name"].toString(),
+                    serverData["ble_token"].toString()
+                )
+                this.header = headerClass
+                Triple(true, userClass, hashMapOf("" to ""))
+            }
+            is Result.Failure -> {
+                Triple(false, user, hashMapOf("" to ""))
+            }
+        }
+    }
+
+    fun signUp(user: UserClass, DB: DatabaseHelper): Triple<Boolean, UserClass, HashMap<String?, String?>>{
+        val body = Gson().toJson(
+            hashMapOf(
+                "registration" to user
+            )
+        )
+        val (_, response, result) = this.postJson(
+            SIGN_UP_URL,
+            body
+        )
+        val headerClass = HeaderClass(response)
+        when(result){
+            is Result.Success -> {
+                DB.saveUser(user)
+                this.header = headerClass
+                return Triple(true, user, hashMapOf("" to ""))
+            }
+            is Result.Failure -> {
+                when(response.statusCode){
+                    HTTP_CONFRICT -> {
+                        val (_result, _user) = signIn(user)
+                        return when(_result){
+                            true -> {
+                                DB.saveUser(_user)
+                                Triple(_result, _user, hashMapOf("" to ""))
+                            }
+                            false -> {
+                                // TODO: エラー文追加
+                                Triple(false, user, hashMapOf("" to ""))
+                            }
+                        }
+                    }
+                    else -> {
+                        // TODO: エラー文追加
+                        return Triple(false, user, hashMapOf("" to ""))
+                    }
+                }
+            }
+        }
     }
 }
 
@@ -155,6 +302,19 @@ class BluetoothClass(val context: Context){
     }
 }
 
+class UserClass(email: String, password: String, name: String, ble_token: String = String.format("%09d", (0..0xFFFFFFF).random())){
+    val email: String? = email
+    val password: String? = password
+    val name: String? = name
+    val ble_token: String? = ble_token
+}
+
+class HeaderClass(response: Response){
+    var uid: String? = response["uid"].toString().removePrefix("[").removeSuffix("]")
+    var token: String? = response["access-token"].toString().removePrefix("[").removeSuffix("]")
+    var client: String? = response["client"].toString().removePrefix("[").removeSuffix("]")
+}
+
 class BleScanReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
         val error = intent!!.getIntExtra(BluetoothLeScanner.EXTRA_ERROR_CODE, -1)
@@ -208,5 +368,95 @@ class BleScanService : Service(){
         startForeground(101, notification)
 
         return START_NOT_STICKY
+    }
+}
+
+class DatabaseHelper(context: Context) : SQLiteOpenHelper(context, DB_NAME, null, DB_VERSION) {
+    companion object {
+        private val DB_VERSION = 1
+        private val DB_NAME = "myapp_dev.db"
+
+        private val TABLE_NAME       = "cafeUser"
+        private val COLUMN_ID        = "id"
+        private val COLUMN_NAME      = "name"
+        private val COLUMN_EMAIL     = "email"
+        private val COLUMN_PASSWORD  = "password"
+        private val COLUMN_BLE_TOKEN = "ble_token"
+
+        private val SQL_CREATE_TABLE = String.format(
+            "CREATE TABLE %s (%s INTEGER PRIMARY KEY AUTOINCREMENT,%s TEXT NOT NULL,%s TEXT NOT NULL, %s TEXT NOT NULL, %s TEXT NOT NULL);",
+            TABLE_NAME,
+            COLUMN_ID,
+            COLUMN_NAME,
+            COLUMN_EMAIL,
+            COLUMN_PASSWORD,
+            COLUMN_BLE_TOKEN
+        )
+
+        private val SQL_DROP_TABLE = String.format("DROP TABLE IF EXISTS %s;", TABLE_NAME)
+    }
+    override fun onCreate(db: SQLiteDatabase?) {
+        db?.execSQL(SQL_CREATE_TABLE)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        db?.execSQL(SQL_DROP_TABLE)
+        onCreate(db)
+    }
+
+    fun saveUser(user: UserClass) {
+        val db = this.writableDatabase
+        val values = ContentValues()
+        values.put(COLUMN_EMAIL, user.email)
+        values.put(COLUMN_NAME, user.name)
+        values.put(COLUMN_PASSWORD, user.password) //TODO: 暗号化する
+        values.put(COLUMN_BLE_TOKEN, user.ble_token)
+        db.insert(TABLE_NAME, null, values)
+        db.close()
+    }
+
+    fun existUser(): Boolean {
+        val db = this.writableDatabase
+        val userSQL = "SELECT count(*) as cnt from $TABLE_NAME;"
+        val cursor: Cursor = db.rawQuery(userSQL, null)
+        cursor.moveToFirst()
+        val count: Int = cursor.getInt(cursor.getColumnIndex("cnt"))
+        cursor.close()
+        return (count > 0)
+    }
+
+    fun getUser(): UserClass {
+        val db = this.writableDatabase
+        val userSQL = "SELECT * from $TABLE_NAME WHERE ID = (SELECT MAX(ID)  FROM $TABLE_NAME);"
+        val cursor: Cursor = db.rawQuery(userSQL, null)
+        var email: String = ""
+        var password: String = ""
+        var name: String = ""
+        var ble: String = ""
+        lateinit var user: UserClass
+        try{
+            when {
+                cursor.moveToNext() -> {
+                    email = cursor.getString(cursor.getColumnIndex(COLUMN_EMAIL))
+                    name = cursor.getString(cursor.getColumnIndex(COLUMN_NAME))
+                    password = cursor.getString(cursor.getColumnIndex(COLUMN_PASSWORD))
+                    ble  = cursor.getString(cursor.getColumnIndex(COLUMN_BLE_TOKEN))
+                }
+            }
+        } finally {
+            user = UserClass(email, password, name, ble)
+            cursor.close()
+        }
+        return user
+    }
+
+    fun existDB(){
+        val db: SQLiteDatabase? = this.writableDatabase
+        this.onUpgrade(db, 1, 1)
+        when (db) {
+            null -> {
+                this.onCreate(db)
+            }
+        }
     }
 }
